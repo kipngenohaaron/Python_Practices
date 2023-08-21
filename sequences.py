9d41227fe67523ce0e80e0db3f79ada6a445861b
kipngenohaaron <kipngenohaaron@gmail.com>
2023-08-21 14:29:36 +0300

    convert an integer to a string

--- sequences.py
+++ sequences.py
@@ -22,3 +22,27 @@ print_fibonacci(10)  # Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 string_number = "123"
 integer_number = int(string_number)
 print(integer_number)  # This will print 123
+
+
+num = '10'
+ 
+# check and print type num variable
+print(type(num))
+ 
+# convert the num from string into int
+converted_num = int(num)
+ 
+# print type of converted_num
+print(type(converted_num))
+ 
+# We can check by doing some mathematical operations
+print(converted_num + 20)
+
+# Output
+# <class 'str'>
+# <class 'int'>
+# 30
+#convert an integer to a string
+integer_number = 123
+string_number = str(integer_number)
+print(string_number)  # This will print "123"
